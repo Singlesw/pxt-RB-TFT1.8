89102c0ed2aa456a367aba3ea28b78353d96287b
Singlesw <44473010+Singlesw@users.noreply.github.com>
2022-11-07 17:14:48 +0000

    Update RB-TFT18.ts

--- RB-TFT18.ts
+++ RB-TFT18.ts
@@ -199,7 +199,7 @@ enum Color {
          // [0xA8] (my+mv+rgb)
          // [0x08] (rgb)
          // [0x68] (mx+mv+rgb)
-         send(TFTCommands.MADCTL, [0x28])
+         send(TFTCommands.MADCTL, [0xA8])
 
          // Set 16-bit color mode
          send(TFTCommands.COLMOD, [0x05])
